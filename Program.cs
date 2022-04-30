44cb6508f3838b9f5829a219caa7e6c9779f94e7
Aaron Singleterry <aaronsingleterry10@gmail.com>
2022-04-30 14:27:27 -0500

    completed lecture

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,18 @@ namespace BoxingUnBoxing
     {
         static void Main(string[] args)
         {
+            var list = new ArrayList();
+            list.Add(1);
+            list.Add("Aaron");
+            list.Add(DateTime.Today);
+
+            var number = (int)list[1];
+
+            var anotherList = new List<int>();
+            anotherList.Add(1);
+
+            var names = new List<string>();
+            names.Add("Aaron");
         }
     }
 }
